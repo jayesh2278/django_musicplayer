4bf192ce8b0724c1b0db6440bf5601162598bc70
jayesh <jayeshsakhiya10@gmail.com>
2022-07-31 23:10:11 +0530

    music player with skip count chnage

---
+++ README.md
@@ -0,0 +1,15 @@
+# music_player_django
+
+this is music player web-app made from python django framework
+
+follow the below step:
+
+1. create env 
+2. pip install -r requirements.txt
+3. python manage.py makemigrations
+4. python manage.py migrate
+5. python manage.py runserver
+
+![Screenshot 2022-07-27 at 1 31 50 AM](https://user-images.githubusercontent.com/94175361/181101658-66850ec6-2351-4272-8c8b-84421846b23f.png)
+
+here we use redis sessions for count skip button according to ip adress.
---
+++ app/views.py
@@ -0,0 +1,66 @@
+# Create your views here.
+from functools import cache
+from urllib import response
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+# imported our models
+from django.core.paginator import Paginator
+from .models import Song
+from django.core.cache import cache
+import redis
+from django.conf import settings
+import random
+
+
+def index(request):
+        paginator= Paginator(Song.objects.all(),1)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        # context={"page_obj":page_obj}
+
+        return render(request,"index.html",{'page_obj':page_obj,}) 
+
+# def comlr(request,id):
+#     son = Song.objects.get(pk=id) 
+#     pko = Skipcount.objects.create(song=son)
+#     songcount = Skipcount.objects.filter(song=id).count()
+#     print(songcount)    
+#     return render(request, "counter.html",{"songcount":songcount})
+
+r = redis.Redis(host=settings.REDIS_HOST,
+                   port=settings.REDIS_PORT,
+                   db=settings.REDIS_DB)      
+
+def coml(request,id):
+    # for getting ip adress of user who click skip button
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        # print "returning FORWARDED_FOR"
+        ip = x_forwarded_for.split(',')[-1].strip()
+    elif request.META.get('HTTP_X_REAL_IP'):
+        # print "returning REAL_IP"
+        ip = request.META.get('HTTP_X_REAL_IP')
+    else:
+        # print "returning REMOTE_ADDR"
+        ip = request.META.get('REMOTE_ADDR')
+    print(ip)
+    number = random.randint(0,255)
+    ip = f'192.168.0.{number}'
+    # 
+    # redis operations
+    user_key = f"{ip}:{id}"
+    skip_counter = f"skipper:{id}"
+    response = r.get(user_key)
+    if response is None:
+        print("Updating counter")
+        r.set(user_key, "SKIPPED", keepttl=True)
+        r.expire(user_key, 600)
+        r.incr(skip_counter,1)
+        r.expire(skip_counter, 600*5)
+    else:
+        print("not updating same ip found!!")    
+    total_skip =  r.get(skip_counter)
+    print(total_skip)
+
+    return HttpResponse("ok")
